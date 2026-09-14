f2fd353e85aa65ec6aac8a0291052677e38741d5
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-09-14 18:28:07 -0400

    Update storage.hpp

--- include/v4q128/core/storage.hpp
+++ include/v4q128/core/storage.hpp
@@ -1,7 +1,23 @@
 #pragma once
+
 #include <immintrin.h>
 #include <cstdint>
 
 namespace v4q128 {
-    // Phase 1 storage struct will be placed here
-}
+
+struct alignas(32) v4q128 {
+    __m256i lo;
+    __m256i hi;
+
+    v4q128() = default;
+
+    constexpr v4q128(__m256i low_limbs, __m256i high_limbs) noexcept
+        : lo(low_limbs), hi(high_limbs) {}
+
+    [[nodiscard]] static inline v4q128 zero() noexcept {
+        __m256i z = _mm256_setzero_si256();
+        return v4q128(z, z);
+    }
+};
+
+} // namespace v4q128
