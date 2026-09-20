964070c64351baaa961dd5ad05215574ba6e6636
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-09-20 16:46:33 -0400

    implemented step 2 mul.hpp
    implemented step 2, created res_lo for the entire mul function. basically started building the base fractional result, added the stuff from weight 2^64

--- include/v4q128/ops/mul.hpp
+++ include/v4q128/ops/mul.hpp
@@ -40,15 +40,34 @@ namespace v4q128 {
     __m256i b2 = b_hi_abs;
     __m256i b3 = _mm256_srli_epi64(b_hi_abs, 32);
 
+    // step 1
     __m256i a0_b0 = _mm256_mul_epu32(a0, b0);
     __m256i a0_b1 = _mm256_mul_epu32(a0, b1);
     __m256i a1_b0 = _mm256_mul_epu32(a1, b0);
+   
     __m256i zero = _mm256_setzero_si256();
     __m256i a0b0_align = _mm256_srli_epi64(a0_b0, 32);
     __m256i a0_b1_32 = _mm256_blend_epi32(a0_b1, zero, 0xAA);
     __m256i a1_b0_32 = _mm256_blend_epi32(a1_b0, zero, 0xAA);
+    
     __m256i sum_32 = _mm256_add_epi64(a0_b0_align, _mm256_add_epi64(a0_b1_32, a1_b0_32));
     __m256i bit_64_carry = _mm256_srli_epi64(sum_32, 32);
+
+    // step 2
+    __m256i a0_b2 = _mm256_mul_epu32(a0, b2);
+    __m256i a1_b1 = _mm256_mul_epu32(a1, b1);
+    __m256i a2_b0 = _mm256_mul_epu32(a2, b0);
+
+    __m256i a0b1_align = _mm256_srli_epi64(a0_b1, 32);
+    __m256i a1b0_align = _mm256_srli_epi64(a1_b0, 32);
+    __m256i res_lo_base = _mm256_add_epi64(bit_64_carry, _mm256_add_epi64(a0b1_align, a1b0_align));
+
+    __m256i sum_pair1 = _mm256_add_epi64(a0_b2, a1_b1);
+    __m256i sum_pair2 = _mm256_add_epi64(a2_b0, res_lo_base);
+    __m256i res_lo = _mm256_add_epi64(sum_pair1, sum_pair2);
+    
+    
+    
     
 
     
